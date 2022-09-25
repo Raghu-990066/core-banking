fd8555c959a90281972eb4af865ad513dbe75b44
amdjfsonb72 <amdjfsonb72@Amdsepjfs306.iiht.tech>
2022-09-25 22:30:28 +0530

    first push

---
+++ src/com/training/amdocs/BaseClass.java
@@ -0,0 +1,10 @@
+package com.training.amdocs;
+
+public class BaseClass {
+	public static void main(String args[]) {
+		System.out.println("hello world!!");
+		System.out.println("this is raghunandan ");
+	}
+
+
+}
