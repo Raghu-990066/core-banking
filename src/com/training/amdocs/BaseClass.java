90d8e1d3924763685d0eb633ea5b7f550ac15d27
amdjfsonb72 <amdjfsonb72@Amdsepjfs306.iiht.tech>
2022-09-28 00:07:21 +0530

    final commit 1

--- src/com/training/amdocs/BaseClass.java
+++ src/com/training/amdocs/BaseClass.java
@@ -17,6 +17,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class BaseClass {//full
@@ -68,10 +69,13 @@ public class BaseClass {//full
 				    	   String user_address=s.next();
 				    	
 				    	   System.out.println("Enetr a opening balance");
-				    	   double user_bal=s.nextDouble();				    				    	     
+				    	   double user_bal=s.nextDouble();		
+				    	   Random rnd=new Random();
+							String num="10" +rnd.nextInt(99);
+							int pass=Integer.parseInt(num);
 			    	        Statement st1=con.createStatement();
 			    	        ResultSet rs1=st1.executeQuery("SELECT * FROM (select acc_no from bank_base_user ORDER BY acc_no DESC) where rownum = 1 ");
-			    	       
+			    	        
 			    	        while(rs1.next()) {
 			    	        	int in_number;
 			    	        	System.out.println(rs1.getString(1));
@@ -79,21 +83,26 @@ public class BaseClass {//full
 						    	in_number = rs1.getInt(1);
 						    	in_number += 1;
 						    	System.out.println(in_number);
+						    	
 						    	    	        			    	        
 				             if(con !=null) {
 							    	
 							    PreparedStatement ps;
-								ps=con.prepareStatement("insert into bank_base_user values(?,?,?,?,?)");
+								ps=con.prepareStatement("insert into bank_base_user values(?,?,?,?,?,?)");
 							    ps.setInt(1, in_number);
 								ps.setString(2, user_name);
 								
 								ps.setString(3, phone_no);
 								ps.setString(4, user_address);
 								ps.setDouble(5, user_bal);
+								ps.setInt(6, pass);
 								
 								int i=ps.executeUpdate();
-								if (i==1)
+								if (i==1) {
 									System.out.println("record inserted successfully and get an account number is  "+in_number);
+									System.out.println("password is : "+pass);
+									System.out.println("PLEASE DO NOT SHARE PASSWORD WITH ANY ONE");
+								}
 								else
 								  System.out.println("invalid insertion");
 							    }				    	  
@@ -124,12 +133,48 @@ public class BaseClass {//full
 				       case 3://Case withdraw
 				    	   System.out.println("Enter a account number");
 		    	           int acc_no_withdraw=s.nextInt();
+		    	           System.out.println("Enter password of account"+acc_no_withdraw);
+		    	           int acc_pass=s.nextInt();
 		    	           System.out.println("Enter amount to withdraw");
 		    	           int amt_withdraw=s.nextInt();
+		    	           int pass_from_db=0;
+		    	           
+		    	           
+		    	           Statement st5=con.createStatement();
+		    	           ResultSet rs5=st5.executeQuery("select password from bank_base_user where acc_no="+acc_no_withdraw);
+		    	           
+//			    	        System.out.println("Account Number = " + acc_no_withdraw);
+			    	        while(rs5.next()) {
+//			    	        System.out.println("password : "+rs5.getInt(1));
+			    	        pass_from_db +=rs5.getInt(1);
+			    	        }
+			    	        
+			    	        
+			    	        
+			    	        
+			    	        
+			    	        
+		    	           if (acc_pass == pass_from_db) {
+//		    	        	   System.out.println("inside if");
+		    	           
+		    	           
 		    	           PreparedStatement ps2=con.prepareStatement("update bank_base_user set account_bal=account_bal-? where acc_no=?");
 		    	           ps2.setInt(1, amt_withdraw);
 		    	           ps2.setInt(2, acc_no_withdraw);
 		    	           ps2.executeUpdate();
+		    	           System.out.println("withdraw done");
+		    	           }
+		    	           
+		    	           else {
+		    	        	   System.out.println("enter valid password");
+		    	           }
+		    	           
+			    	        
+		    	           rs5.close();
+		    	           
+		    	           
+		    	           
+		    	           
 		    	           
 		    	           
 		    	           Statement st3=con.createStatement();
@@ -139,6 +184,9 @@ public class BaseClass {//full
 			    	       System.out.println("Current Balance : "+rs3.getString(1));
 			    	       }
 			    	       rs3.close();
+			    	       
+			    	        
+		    	        	   
 		    	           break; 	
 				    	    
 				      case 4://Case bal
